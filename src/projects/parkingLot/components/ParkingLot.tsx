528a7d046245dcd8b3fd5bc65eff410c14386ee8
nareshb7 <nareshbjava7@gmailcom>
2025-03-25 15:25:58 +0530

    components created

---
+++ src/projects/parkingLot/components/ParkingLot.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ParkingLot = () => {
+  return (
+    <div>ParkingLot</div>
+  )
+}
+
+export default ParkingLot
---
+++ src/projects/parkingLot/components/VehicleEntry.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VehicleEntry = () => {
+  return (
+    <div>VehicleEntry</div>
+  )
+}
+
+export default VehicleEntry
